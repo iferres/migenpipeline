9b62ae2bb6f3a97115d0761f736cc23b9fbb8040
iferres <igferres@gmail.com>
2017-11-23 13:02:57 -0200

    runGubbins outputs a vector with the filenames

--- R/runGubbins.R
+++ R/runGubbins.R
@@ -9,4 +9,15 @@ runGubbins <- function(fastaCore, prefix, gargs='--threads 4 -t fasttree'){
   gubbins <- paste('run_gubbins.py', gargs, fastaCore)
   system(gubbins)
 
+  out <- c()
+  out[1] <- paste0(prefix, '.branch_base_reconstruction.embl')
+  out[2] <- paste0(prefix, '.recombination_predictions.gff')
+  out[3] <- paste0(prefix, '.recombination_predictions.embl')
+  out[4] <- paste0(prefix, '.filtered_polymorphic_sites.phylip')
+  out[5] <- paste0(prefix, '.summary_of_snp_distribution.vcf')
+  out[6] <- paste0(prefix, '.per_branch_statistics.csv')
+  out[7] <- paste0(prefix, '.filtered_polymorphic_sites.fasta')
+  out[8] <- paste0(prefix, '.node_labelled.final_tree.tre')
+  out[9] <- paste0(prefix, '.final_tree.tre')
+
 }
